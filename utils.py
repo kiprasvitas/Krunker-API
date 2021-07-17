03e3211148bb0467ed77a0fa939d3082e8a196f5
Kipras <kipraszapas@gmail.com>
2021-07-16 21:35:23 -0400

    v2

--- utils.py
+++ utils.py
@@ -12,8 +12,8 @@ import sys
 
 def scrapeUser(name):
     user_name = name
-    email = "YOUR HCAPTCHA ACCOUNT EMAIL"
-    password = "YOUR HCAPTCHA ACCOUNT PASSWORD"
+    email = os.environ.get("EMAIL")
+    password = os.environ.get("PASS")
 
     options = uc.ChromeOptions()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -23,7 +23,7 @@ def scrapeUser(name):
     options.add_argument("--no-sandbox")
 
     #executable_path=os.environ.get("CHROMEDRIVER_PATH"), 
-    driver = uc.Chrome(keep_alive=True, version_main=90, executable_path=os.environ.get("CHROMEDRIVER_PATH"), patcher_force_close=True, options=options, enable_cdp_events=True)
+    driver = uc.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), patcher_force_close=True, options=options)
 
     def solveCaptcha():
         driver.get("https://dashboard.hcaptcha.com/login")
@@ -32,8 +32,8 @@ def scrapeUser(name):
         driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div[3]/div[2]/input").send_keys(password)
         driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[4]/button").click()
         time.sleep(2.2)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[3]/button"))).click()
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[3]/span")))
+        WebDriverWait(driver, 16).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[3]/button"))).click()
+        WebDriverWait(driver, 16).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[3]/span")))
         time.sleep(2)
 
 
@@ -54,6 +54,7 @@ def scrapeUser(name):
     captcha = iscaptcha()
 
     if captcha == True:
+        print("found captcha")
         solveCaptcha()
         driver.get('https://krunker.io/social.html?p=profile&q={}'.format(user_name))
         time.sleep(2)
@@ -67,42 +68,53 @@ def scrapeUser(name):
     time.sleep(1)
 
     try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "statHolder")))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="I Accept"]'))).click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "statHolder")))
         res = driver.find_element_by_id('statHolder')
         res_html = res.get_attribute('innerText')
 
         is_hacker = driver.find_element_by_id('hackText')
         if (is_hacker.get_attribute('style') == "display: none;"):
-            hacker = "FALSE"
+            hacker = "false"
         else:
-            hacker = "TRUE"
+            hacker = "true"
 
-        followers = driver.find_element_by_xpath('/html/body/div[2]/div[8]/div[1]/div[3]/div[3]/div[1]').get_attribute('innerText')[0:-9]
-        following = driver.find_element_by_xpath('/html/body/div[2]/div[8]/div[1]/div[3]/div[3]/div[2]').get_attribute('innerText')[0:-9]
+        followers = driver.find_elements_by_class_name('follL')[0].get_attribute('innerText')[0:-9]
+        following = driver.find_elements_by_class_name('follL')[1].get_attribute('innerText')[0:-9]
 
-        def check_exists_by_xpath(xpath):
+        def checkClan():
             try:
-                driver.find_element_by_xpath(xpath)
-            except:
-                return "FALSE"
-            return "TRUE"
+                links = driver.find_elements_by_tag_name('a')
+                for link in links:
+                    if link.get_attribute("href").startswith("https://krunker.io/social.html?p=clan&q="):
+                        href = link.get_attribute("href")
+                        playerclan = href[href.find("q")+2:]
+                        return playerclan
+            except Exception as e:
+                print(e)
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
+                return False
 
         def check_badge(name):
             badges = []
-            for icon in driver.find_element_by_xpath("/html/body/div[2]/div[8]/div[1]/div[3]/div[1]").find_elements_by_class_name('material-icons'):
+            for icon in driver.find_element_by_id("rightProfile").find_element_by_class_name('leftTopHolder').find_elements_by_class_name('material-icons'):
                 if (icon.get_attribute("innerText") == name):
                     badges.append("true")
                 else:
                     badges.append("false")
             if any("true" in s for s in badges):
-                return "TRUE"
+                return "true"
             else:
-                return "FALSE"
+                return "false"
 
-        if (check_exists_by_xpath("/html/body/div[2]/div[8]/div[1]/div[3]/div[1]/a") == "TRUE"):
-            clan = driver.find_element_by_xpath("/html/body/div[2]/div[8]/div[1]/div[3]/div[1]/a").get_attribute('innerText')
+        isclan = checkClan()
+
+        if isclan != False:
+            clan = isclan
         else:
-            clan = "NONE"
+            clan = "none"
 
         verified = check_badge("check_circle")
         partner = check_badge("verified")
@@ -114,15 +126,18 @@ def scrapeUser(name):
             res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
             return res_dct
         sentence_dict = Convert(words_list)
-        sentence_dict['HACKER'] = hacker
-        sentence_dict['CLAN'] = clan
-        sentence_dict['FOLLOWERS'] = followers
-        sentence_dict['FOLLOWING'] = following
-        sentence_dict['PARTNER'] = partner
-        sentence_dict['PREMIUM'] = premium
-        sentence_dict['VERIFIED'] = verified
+        sentence_dict['hacker'] = hacker
+        sentence_dict['clan'] = clan
+        sentence_dict['followers'] = followers
+        sentence_dict['following'] = following
+        sentence_dict['partner'] = partner
+        sentence_dict['premium'] = premium
+        sentence_dict['verified'] = verified
         sentence_dict = json.dumps(sentence_dict)
-        return sentence_dict
-    except:
+        return sentence_dict.lower()
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
         driver.quit()
         return "No user found with this name."
